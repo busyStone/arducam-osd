05b9a3c76aaed9bce284ef06dfd78100e2fd41f8
busyStone <jlu.xzp@gmail.com>
2016-05-26 14:09:14 +0800

    define firmware version

--- ArduCAM_OSD/ArduCAM_OSD.ino
+++ ArduCAM_OSD/ArduCAM_OSD.ino
@@ -95,6 +95,17 @@ OSD osd; //OSD object
 
 SimpleTimer  mavlinkTimer;
 
+enum FIRMWARE_VERSION_TYPE
+{
+    FIRMWARE_VERSION_TYPE_DEV=0, /* development release | */
+    FIRMWARE_VERSION_TYPE_ALPHA=64, /* alpha release | */
+    FIRMWARE_VERSION_TYPE_BETA=128, /* beta release | */
+    FIRMWARE_VERSION_TYPE_RC=192, /* release candidate | */
+    FIRMWARE_VERSION_TYPE_OFFICIAL=255, /* official stable release | */
+    FIRMWARE_VERSION_TYPE_ENUM_END=256, /*  | */
+};
+#define THISFIRMWARE "SkyBorne:OSD v1.0.1"
+#define FIRMWARE_VERSION 1,0,1,FIRMWARE_VERSION_TYPE_OFFICIAL
 
 /* **********************************************/
 /* ***************** SETUP() *******************/
